36725a241f8432552eebcb5a458f57dfa4a9f678
Jackson Kunde <jacksonkunde@gmail.com>
2024-12-03 12:48:27 -0800

    updating package management __init__.py

--- src/ireason/__init__.py
+++ src/ireason/__init__.py
@@ -5,7 +5,7 @@ from .base.solution_evaluator import SolutionEvaluator
 from .base.exporter import CSVExporter, JSONExporter, HuggingFaceExporter
 
 # iGSM
-from .iGSM.data_generator import GSMDataGenerator
+# from .iGSM.data_generator import GSMDataGenerator
 
 # iadd
 from .iadd.addition_data_generator import AdditionDataGenerator
